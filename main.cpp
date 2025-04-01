80f67e2255fdd8952749dccbf7f4b0acd4fa6fc5
akelamain <malkovnikituschka@yandex.ru>
2025-04-01 15:49:51 +0300

    trying out sfml

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+int main(){
 
-int main () {
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(300, 300), "SFML WWW!");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
 }
